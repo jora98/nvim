f8a3434001abd93791b2a50e0803acac42d6190e
jora98 <jona@familiermas.de>
2023-11-03 22:11:50 +0100

    transparent nvim

--- lua/jona/plugins/colorscheme.lua
+++ lua/jona/plugins/colorscheme.lua
@@ -4,5 +4,13 @@ return {
 	config = function()
 		-- load the colorscheme here
 		vim.cmd([[colorscheme gruvbox]])
+		-- Set transparent background
+		vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+		vim.cmd("hi SignColumn guibg=NONE")
+		vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
+		vim.cmd("hi MsgArea guibg=NONE ctermbg=NONE")
+		vim.cmd("hi TelescopeBorder guibg=NONE ctermbg=NONE")
+		vim.cmd("hi NvimTreeNormal guibg=NONE ctermbg=NONE")
+		vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE")
 	end,
 }
